471ba9dec0c8bdd74189a06e27b981a2f4f74a5c
Lewis Nakao <lewis.nakao@polarisalpha.com>
2021-01-31 01:47:52 -1000

    Replace landing page

---
+++ site/assets/projects.js
@@ -0,0 +1,71 @@
+const categories = [
+  { name: "Apps", emoji: "📲" },
+  { name: "Games", emoji: "👾" },
+  { name: "Tenrikyo", emoji: "🙏" },
+];
+
+const projects = [
+// Apps
+{
+  title: "Math Homework Generator", name: "hw-gen", emoji: '📝',
+  description: "Randomly generates printable worksheets.",
+  category: "Apps",
+},
+{
+  title: "Meta Tag Generator", name: "meta-tag-gen", emoji: '🤖',
+  description: "Quickly generate your standard meta tags for social media and SEO.",
+  category: "Apps",
+},
+{
+  title: "Regex Find & Format Tool", url: "https://lewdev.github.io/apps/regex-find-and-format", emoji: '🔍',
+  description: "Perform regular expression search & replace operations on text.",
+  category: "Apps",
+},
+{
+  title: "Google Translate Link Generator", name: "trans", emoji: '⛓️',
+  description: "A Google Translate link generator with link history.",
+  category: "Apps",
+},
+// Games
+{
+  title: "Moonshot Commander", name: "moonshot-commander", emoji: '🕹️',
+  description: "Released 11/30/2020 for Game Off 2020",
+  category: "Games",
+},
+{
+  title: "Burger Builder", name: "burger-builder", emoji: '🍔',
+  description: "Quickly build the ordered burgers before the time runs out.",
+  category: "Games",
+},
+{ title: "Number Guessing Game", name: "number-guess", emoji: '🤔',
+  description: "Guess your number while it tracks your guesses and time. Has multiple difficulties.",
+  category: "Games",
+},
+// { title: "Tick-Tack-Toe", name: "ticktacktoe",  emoji: '⭕',
+//   url: "https://lewdev.github.io/apps/ticktacktoe/v1/" },
+
+// Tenrikyo
+{
+  title: "Ofudesaki Browser", url: "https://lewdev.github.io/ofudesaki-browser/", emoji: '📕',
+  description: "Browse and search a Tenrikyo scripture, the Ofudesaki.",
+  category: "Tenrikyo",
+},
+{
+  title: "Narimono App (prototype)", name: "narimono", emoji: '🎶', url: "https://lewdev.github.io/apps/narimono-prototype/",
+  description: "A sound-board app of the instruments used in the Tenrikyo service. An unfinished project that was going to be a rhythm game.",
+  category: "Tenrikyo",
+},
+{
+  title: "Kanrodai App", name: "kanrodai-app", emoji: '🗺️',
+  description: "Shows the location of the Kanrodai, a sacred location, relative to your location on a map.",
+  category: "Tenrikyo",
+},
+];
+
+const meLinks = [
+{ title: "My resume", url: "https://docs.google.com/document/d/e/2PACX-1vSE8xfQwOKlVGXZPtW9wMp8-vwlNVz_z6LnrFje_E25GfbQjWWMgyNzQ1e4CjSfDWrsVtUGMhKuE9pU/pub"},
+{ title: "Github Profile", url: "https://github.com/lewdev",},
+{ title: "LinkedIn Profile", url: "https://www.linkedin.com/in/lewisnakao" },
+{ title: "StackOverflow", url: "http://stackoverflow.com/cv/lewis.nakao" },
+{ title: "Sponsor Me", url: "" },
+];
